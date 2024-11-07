d715524071a3ad822d69cce77e78c091f670d450
Muaaz Shaikh <mizerablepi@gmail.com>
2024-11-08 01:58:06 +0300

    anam ka gift

--- signup/delete.php
+++ signup/delete.php
@@ -1,4 +1,15 @@
 <?php
 include("connect.php");
-$id = $_GET['id'];
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $query = "DELETE FROM regestration WHERE id = $id";
+  $data = mysqli_query($con, $query);
+  if ($data) {
+    echo "<h2>Record deleted successfully</h2>";
 ?>
+    <meta http-equiv="refresh" content="3; url = http://localhost/project/signup/home.php" />
+<?php
+  } else {
+    echo "Failed to delete record";
+  }
+}
